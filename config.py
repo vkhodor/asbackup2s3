562def885f0adfa276651af7c3f5c73ab514718f
Viktor V. Khodorchenko <vkhodor@gmail.com>
2020-12-04 13:23:25 +0700

    cfg fix

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ SERVERS = {
       's3_bucket': 'personartb-backup',
       's3_path': 'prod/us-aerospike-cluster',
       'remote_store_size': '500Gb',
-      'remove_local': True,
-      'estimated_size': 123123
+      'remove_local': False,
+      'estimated_size': 6522147200
     },
     'trackdata': {
       'nice': 30,
