bd4618ae4ccbd9b61aea4b9d48a97cdf82b2d12d
Viktor V. Khodorchenko <vkhodor@gmail.com>
2020-12-09 15:23:35 +0700

    fix

--- s3.py
+++ s3.py
@@ -78,7 +78,6 @@ def keys(s3_client, bucket_name, prefix='/', delimiter='/', start_after=''):
     start_after = (start_after or prefix) if prefix.endswith(delimiter) else start_after
     for page in s3_paginator.paginate(Bucket=bucket_name, Prefix=prefix, StartAfter=start_after):
         for content in page.get('Contents', ()):
-            print('[DBG] {0}'.format(content))
             yield S3Key(content['Key'], content['LastModified'], content['ETag'], content['Size'], content['StorageClass'])
 
 
